40b55871358a87d849e6c273134e11f9e9cf44fa
Enzo <enzo.aime91@gmail.com>
2024-11-18 19:16:31 +0100

    log info connection

--- organize-me-back/index.js
+++ organize-me-back/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const cors = require('cors');
-const morgan = require('morgan');
-const router = require('./routes'); // Import des routes principales
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("./routes"); // Import des routes principales
 
 // Initialisation de l'application
 const app = express();
 
 // Middleware pour les logs des requêtes
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Middleware pour gérer les JSON et limiter leur taille
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 // Middleware CORS
 app.use(
   cors({
     origin: [
-      'http://localhost:5173', // Front-end local
-      'https://organize-me-front.vercel.app', // Front-end sur Vercel
+      "http://localhost:5173", // Front-end local
+      "https://organize-me-front.vercel.app", // Front-end sur Vercel
     ],
     credentials: true, // Permet l'envoi de cookies et d'en-têtes d'autorisation
   })
@@ -31,24 +31,35 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('✅ MongoDB Connected'))
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+
+    app.get("/mongodb-test", async (req, res) => {
+      try {
+        const dbStatus = await mongoose.connection.db.admin().ping();
+        res.status(200).json({ message: "MongoDB fonctionne bien.", dbStatus });
+      } catch (err) {
+        console.error("Erreur MongoDB :", err);
+        res.status(500).json({ error: "MongoDB ne répond pas." });
+      }
+    });
+  })
   .catch((err) => {
-    console.error('❌ MongoDB Connection Error:', err);
+    console.error("❌ MongoDB Connection Error:", err);
     process.exit(1);
   });
-
 // Routes principales sans préfixe `/api`
-app.use('/', router);
+app.use("/", router);
 
 // Route de test
-app.get('/connected', (req, res) => {
-  res.status(200).send('Bienvenue sur l\'API Organize Me!');
+app.get("/connected", (req, res) => {
+  res.status(200).send("Bienvenue sur l'API Organize Me!");
 });
 
 // Middleware pour les erreurs
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Une erreur interne est survenue.' });
+  res.status(500).json({ error: "Une erreur interne est survenue." });
 });
 
 // Démarrer le serveur
