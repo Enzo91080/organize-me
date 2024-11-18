9797be60bf08db1e083a9a8e4c2f92354bd9c18d
Enzo <enzo.aime91@gmail.com>
2024-11-18 01:10:10 +0100

    update cors

--- organize-me-back/index.js
+++ organize-me-back/index.js
@@ -17,7 +17,7 @@ app.use(express.json({ limit: "50mb" }));
 // Définir les origines autorisées
 const allowedOrigins = [
   'http://localhost:5173', // Front-end en local
-  'https://organize-me-brown.vercel.app', // Front-end déployé sur Vercel
+  'https://organize-me-front.vercel.app', // Front-end déployé sur Vercel
 ];
 
 const corsOptions = {
